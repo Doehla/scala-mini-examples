d63266d7d9e083e6ecf2107eb3e426548b5926e5
Doehla <jeremie.doehla@gmail.com>
2021-11-08 14:35:49 -0800

    initial code checkin

---
+++ examples/apiWithHttp4s/src/main/scala/Main.scala
@@ -0,0 +1,39 @@
+import cats.effect.{IOApp, IO, ExitCode}
+import org.http4s.HttpRoutes
+import org.http4s.dsl.io._
+import org.http4s.blaze.server.BlazeServerBuilder
+import org.http4s.{EntityDecoder, EntityEncoder}
+import org.http4s.circe.{jsonOf, jsonEncoderOf}
+import io.circe.syntax.EncoderOps
+import com.typesafe.scalalogging.Logger
+
+import example.User
+
+
+object Main
+  extends IOApp {
+  val logger = Logger("Main")
+
+  implicit val userDecoder: EntityDecoder[IO, User] = jsonOf[IO, User]
+
+  val httpApp = HttpRoutes.of[IO] {
+    case req@POST -> Root / "hello" => for {
+      usr <- req.as[User]
+      _ = doThing(usr)
+      res <- Ok(usr.asJson.noSpaces)
+    } yield res
+  }.orNotFound
+
+  def doThing(usr: User): Unit = {
+    logger.info(s"${usr.toString}; ${usr.asJson.noSpaces}")
+  }
+
+  def run(args: List[String]): IO[ExitCode] =
+    BlazeServerBuilder[IO]
+      .bindHttp(8080, "localhost")
+      .withHttpApp(httpApp)
+      .serve
+      .compile
+      .drain
+      .as(ExitCode.Success)
+}
---
+++ examples/apiWithHttp4s/src/main/scala/User.scala
@@ -0,0 +1,21 @@
+package example
+
+import io.circe.{Decoder, Encoder, HCursor}
+import io.circe.syntax.EncoderOps
+import io.circe.Json
+
+case class User(
+  name: String
+)
+
+object User {
+  implicit val decoder: Decoder[User] = (c: HCursor) => for {
+    name <- c.get[String]("name")
+  } yield {
+    User(name)
+  }
+
+  implicit val encoder: Encoder[User] = (a: User) => Json.obj(
+    ("name", a.name.asJson)
+    )
+}
