867786344798e1be477080c17f6b1aa784a0b7fc
Doehla <jeremie.doehla@gmail.com>
2022-08-29 11:15:34 -0700

    Update FileHelper.scala

--- module/dataEngTools/src/main/scala/com/styleseat/dataEngTools/FileHelper.scala
+++ module/dataEngTools/src/main/scala/com/styleseat/dataEngTools/FileHelper.scala
@@ -6,6 +6,8 @@ import scala.io.{BufferedSource, Source}
 
 object FileHelper {
 
+  // TODO: File operations are IO operations... this needs to be updated for IO
+
   def gzipFileInputStream(s: String): BufferedSource =
     Source.fromInputStream(
       new GZIPInputStream(new BufferedInputStream(new FileInputStream(s)))
