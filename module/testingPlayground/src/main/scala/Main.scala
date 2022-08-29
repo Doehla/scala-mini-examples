4a31372d3e5fa2aa98b7564a2c18a6dc687fbc59
Doehla <jeremie.doehla@gmail.com>
2022-08-29 10:46:12 -0700

    Create Main.scala

---
+++ module/testingPlayground/src/main/scala/Main.scala
@@ -0,0 +1,18 @@
+import cats.effect._
+import java.io.File
+
+object Main extends IOApp {
+
+  // copy as defined before
+  def copy(origin: File, destination: File): IO[Long] = ???
+
+  override def run(args: List[String]): IO[ExitCode] =
+    for {
+      _      <- if(args.length < 2) IO.raiseError(new IllegalArgumentException("Need origin and destination files"))
+                else IO.unit
+      orig = new File(args(0))
+      dest = new File(args(1))
+      count <- copy(orig, dest)
+      _     <- IO.println(s"$count bytes copied from ${orig.getPath} to ${dest.getPath}")
+    } yield ExitCode.Success
+}
