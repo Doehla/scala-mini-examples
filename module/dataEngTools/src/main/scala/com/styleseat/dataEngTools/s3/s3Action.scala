046bd30a0d6280404338d24deb430d836e0b9cd5
Doehla <jeremie.doehla@gmail.com>
2022-08-29 15:43:32 -0700

    Update s3Action.scala

--- module/dataEngTools/src/main/scala/com/styleseat/dataEngTools/s3/s3Action.scala
+++ module/dataEngTools/src/main/scala/com/styleseat/dataEngTools/s3/s3Action.scala
@@ -1,7 +1,7 @@
 package com.styleseat.dataEngTools.s3
 
 import software.amazon.awssdk.services.s3.S3Client
-import software.amazon.awssdk.services.s3.model.GetObjectRequest
+import software.amazon.awssdk.services.s3.model.{GetObjectRequest, PutObjectRequest}
 import cats.effect._
 import java.nio.file.Files
 import java.io.File
@@ -9,13 +9,20 @@ import java.io.File
 
 object s3Action {
 
-  private val s3: S3Client =
+  private final val s3: S3Client =
     S3Client
       .builder()
       .build()
 
-  private def composeFetchFileRequest(bucket: String, key: String) =
+  private def composeDownloadFileRequest(bucket: String, key: String) =
     GetObjectRequest
+      .builder()
+      .bucket(bucket)
+      .key(key)
+      .build()
+
+  private def composeUploadFileRequest(bucket: String, key: String) =
+    PutObjectRequest
       .builder()
       .bucket(bucket)
       .key(key)
@@ -23,22 +30,43 @@ object s3Action {
 
   def downloadFile(
     obj: s3Object,
-    destination: File
-  ): IO[File] = {
-    downloadFile(obj.bucket, obj.key, destination)
-  }
+    destination: File,
+    overwrite: Boolean = true
+  ): IO[Unit] =
+    downloadFile(obj.bucket, obj.key, destination, overwrite)
 
   def downloadFile(
     bucket: String,
     key: String,
-    destination: File
-  ): IO[File] = {
+    destination: File,
+    overwrite: Boolean = true
+  ): IO[Unit] = {
     for {
       _ <- IO.blocking(Files.createDirectories(destination.toPath.toAbsolutePath.getParent))
-      _ <- IO.blocking(Files.deleteIfExists(destination.toPath))
+      _ <- IO.blocking({
+        if (Files.exists(destination.toPath)) {
+          if (overwrite) Files.deleteIfExists(destination.toPath)
+          else IO.raiseError(new UnsupportedOperationException("Target file already exists & set to not overwrite."))
+        }
+      })
+      // TODO: This should be converted into some logging call...
       _ <- IO.println(s"downloading file: ${bucket}/${key} -> ${destination}")
-      _ <- IO.blocking(s3.getObject(composeFetchFileRequest(bucket, key), destination.toPath).deleteMarker())
-    } yield destination
+      request = composeDownloadFileRequest(bucket, key)
+      _ <- IO.blocking(s3.getObject(request, destination.toPath).deleteMarker())
+    } yield ()
   }
 
+  def uploadFile(
+      obj: s3Object,
+      source: File
+  ): IO[Unit] =
+    uploadFile(obj.bucket, obj.key, source)
+
+  def uploadFile(
+      bucket: String,
+      key: String,
+      source: File
+  ): IO[Unit] = {
+    IO.blocking(s3.putObject(composeUploadFileRequest(bucket, key), source.toPath))
+  }
 }
