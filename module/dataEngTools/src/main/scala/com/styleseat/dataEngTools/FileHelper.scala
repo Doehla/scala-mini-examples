738cf65636bd31c11586cc753b059bcdee0367c2
Doehla <jeremie.doehla@gmail.com>
2022-08-30 10:07:01 -0700

    Update FileHelper.scala

--- module/dataEngTools/src/main/scala/com/styleseat/dataEngTools/FileHelper.scala
+++ module/dataEngTools/src/main/scala/com/styleseat/dataEngTools/FileHelper.scala
@@ -1,12 +1,33 @@
 package com.styleseat.dataEngTools
 
-import java.io.{BufferedInputStream, FileInputStream}
+import cats.effect.{IO, Resource}
+
+import java.io.{BufferedInputStream, File, FileInputStream}
 import java.util.zip.GZIPInputStream
 import scala.io.{BufferedSource, Source}
 
 object FileHelper {
 
   // TODO: File operations are IO operations... this needs to be updated for IO
+
+  def getExtension(f: File): String =
+    f
+      .getName
+      .split("\\.")
+      .drop(1)
+      .mkString(".")
+
+  def readFileToStream(f: File): Resource[IO, BufferedSource] = {
+    val sourceStream = getExtension(f) match {
+      case "gz" => new GZIPInputStream(new BufferedInputStream(new FileInputStream(f)))
+      case _ => new FileInputStream(f)
+    }
+    Resource.make{
+      IO.blocking(Source.fromInputStream(sourceStream))
+    }{
+      inputStream => IO.blocking(inputStream.close()).handleErrorWith(_ => IO.unit)
+    }
+  }
 
   def gzipFileInputStream(s: String): BufferedSource =
     Source.fromInputStream(
